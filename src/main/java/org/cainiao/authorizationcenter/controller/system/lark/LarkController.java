83185b27f32ea30ffc7a10c42e41d12d3a51d4b2
菜鸟 <wdhlzd@163.com>
2024-03-26 17:24:51 +0800

    :bug: 飞书文件夹清单接口
    Signed-off-by: 菜鸟 <wdhlzd@163.com>

--- src/main/java/org/cainiao/authorizationcenter/controller/system/lark/LarkController.java
+++ src/main/java/org/cainiao/authorizationcenter/controller/system/lark/LarkController.java
@@ -11,8 +11,8 @@ import org.cainiao.api.lark.dto.response.LarkDataResponse;
 import org.cainiao.api.lark.dto.response.LarkFile;
 import org.cainiao.api.lark.dto.response.LarkPage;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -32,9 +32,20 @@ public class LarkController {
     @Operation(summary = "查询飞书文件夹下的列表")
     @HasScope({"lark"})
     public LarkDataResponse<LarkPage<LarkFile>> listItemsInFolder(
-        @Parameter(description = "飞书文件夹Token与分页信息", required = true)
-        @RequestBody ListItemsInFolderRequest listItemsInFolderRequest) {
-
-        return larkApi.docs().space().folder().listItemsInFolder(listItemsInFolderRequest).block();
+        @Parameter(description = "文件夹Token") @RequestParam(value = "folder_token") String folderToken,
+        @Parameter(description = "用户 ID 类型") @RequestParam(value = "user_id_type", required = false, defaultValue = "open_id") String userIdType,
+        @Parameter(description = "排序规则") @RequestParam(value = "order_by", required = false, defaultValue = "EditedTime") String orderBy,
+        @Parameter(description = "升序降序") @RequestParam(value = "direction", required = false, defaultValue = "DESC") String direction,
+        @Parameter(description = "分页大小") @RequestParam(value = "page_size", required = false) Integer pageSize,
+        @Parameter(description = "分页标记，第一次请求不填，表示从头开始遍历") @RequestParam(value = "page_token", required = false) String pageToken) {
+        
+        return larkApi.docs().space().folder().listItemsInFolder(ListItemsInFolderRequest.builder()
+            .folderToken(folderToken)
+            .userIdType(userIdType)
+            .orderBy(orderBy)
+            .direction(direction)
+            .pageSize(pageSize)
+            .pageToken(pageToken)
+            .build()).block();
     }
 }
