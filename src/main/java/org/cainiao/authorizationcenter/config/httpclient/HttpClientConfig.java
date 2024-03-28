5bcc2222270b70cdeadce064abdb4c8aaccbac79
菜鸟 <wdhlzd@163.com>
2024-03-28 14:07:42 +0800

    :bug: 配置WebClient，请求时动态根据目标地址加上对应的OAuth2客户端注册access token
    Signed-off-by: 菜鸟 <wdhlzd@163.com>

--- src/main/java/org/cainiao/authorizationcenter/config/httpclient/HttpClientConfig.java
+++ src/main/java/org/cainiao/authorizationcenter/config/httpclient/HttpClientConfig.java
@@ -1,10 +1,15 @@
 package org.cainiao.authorizationcenter.config.httpclient;
 
+import org.cainiao.oauth2.client.core.httpclient.DynamicExchangeFilterFunction;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * <br />
@@ -18,20 +23,18 @@ public class HttpClientConfig {
      * 当授权服务器作为 OAuth2 客户端调用三方 API 时（如飞书）<br />
      * 需要在发请求前在 HTTP Header 上设置 access token
      *
-     * @param authorizedClientManager OAuth2AuthorizedClientManager
+     * @param oAuth2AuthorizedClientManager OAuth2AuthorizedClientManager
      * @return WebClient
      */
     @Bean
-    WebClient webClient(OAuth2AuthorizedClientManager authorizedClientManager) {
-        /*
-         * TODO 目前只考虑了只配置了一个飞书 OAuth2 客户端注册的情况
-         *  有时间重构下，支持有多个客户端注册时动态选择用哪个客户端注册的 access token
-         */
-        ServletOAuth2AuthorizedClientExchangeFilterFunction
-            exchangeFilterFunction = new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
-        return WebClient.builder()
-            // TODO 只有调用三方 API 的 URI 才需要设置 exchangeFilterFunction.defaultRequest()
-            .defaultRequest(exchangeFilterFunction.defaultRequest())
-            .filter(new DynamicExchangeFilterFunction(exchangeFilterFunction)).build();
+    WebClient webClient(OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager) {
+        // TODO 暂时写死，应该配置到数据库中，并且最好与OAuth2客户端应用的配置统一，先走通流程
+        Map<String, Set<String>> registrationIdUrisMap = new HashMap<>();
+        Set<String> uris = new HashSet<>();
+        uris.add("drive");
+        registrationIdUrisMap.put("cn-lark-client", uris); // 飞书
+        return WebClient.builder().filter(new DynamicExchangeFilterFunction("https://open.feishu.cn/open-apis",
+                oAuth2AuthorizedClientManager, registrationIdUrisMap))
+            .build();
     }
 }
