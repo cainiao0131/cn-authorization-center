e769263874cec3b7da41a8f005fcd7b6bd951dac
菜鸟 <wdhlzd@163.com>
2024-10-02 11:23:31 +0800

    去掉Spring缓存配置
    Signed-off-by: 菜鸟 <wdhlzd@163.com>

--- src/main/java/org/cainiao/authorizationcenter/AuthorizationCenterApplication.java
+++ src/main/java/org/cainiao/authorizationcenter/AuthorizationCenterApplication.java
@@ -7,13 +7,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableWebSecurity
-@EnableCaching
 @MapperScan(basePackageClasses = {UserMapper.class, CnClientRegistrationMapper.class})
 @ComponentScan(basePackageClasses = CnClientRegistrationMapperService.class)
 public class AuthorizationCenterApplication {
